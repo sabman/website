3939c77680943ac93bc2da69ea69010c1097b352
Visnu Pitiyanuvath <visnupx@gmail.com>
2011-07-27 23:05:35 -0700

    lower the logging level. #61

--- config/app.js
+++ config/app.js
@@ -173,7 +173,7 @@ auth.helpExpress(app);
 require('../helpers')(app);
 
 app.listen(port);
-app.ws = require('socket.io').listen(app);
+app.ws = require('socket.io').listen(app).set('log level', 2);
 
 app.on('listening', function() {
   require('util').log("listening on 0.0.0.0:" + port + ".");
