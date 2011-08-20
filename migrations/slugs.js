00f48545c7463e84b6ca6e4a8103a20e03287731
Visnu Pitiyanuvath <visnupx@gmail.com>
2011-08-20 14:30:57 -0700

    assign a first set of slugs to work with

--- migrations/slugs.js
+++ migrations/slugs.js
@@ -1,14 +1,15 @@
 db.teams.find({}, {name: 1}).forEach(function(t) {
   var name = t.name
     , slug = generateSlug(name);
-  if (slug === '' || slug.match(/^-+$/))
-    print(slug, slug === '' ? '!!!' : '', name);
+  print(slug, '<-', name);
+  db.teams.update({_id: t._id}, {$set: {slug: slug}});
 });
 
 function generateSlug(s) {
   return s
     .toLowerCase()
     .replace(/[^-a-z0-9]+/g, '-')
-    .replace(/^-|-$/g, '')
-    .substring(0, 63);
+    .replace(/^-/, '')
+    .substring(0, 20)  // arbitrarily below heroku's 30 limit
+    .replace(/-$/, '');
 }
